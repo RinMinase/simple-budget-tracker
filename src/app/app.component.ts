71bae95a466729cc7a198fba40a4c9dba5ccb1d8
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-30 01:17:22 +0800

    Added firebase setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,21 @@
 import { Component } from "@angular/core";
+import { initializeApp } from "firebase/app";
 
 @Component({
 	selector: "app-root",
 	template: `<app-nav></app-nav><router-outlet></router-outlet>`,
 })
-export class AppComponent { }
+export class AppComponent {
+	constructor() {
+		const firebaseConfig = {
+			apiKey: "AIzaSyAqBGJy6mRnHjhU_YmeebSODruhfYHClj4",
+			authDomain: "budget-tracker-50645.firebaseapp.com",
+			databaseURL: "https://budget-tracker-50645.firebaseio.com",
+			projectId: "budget-tracker-50645",
+			storageBucket: "budget-tracker-50645.appspot.com",
+			messagingSenderId: "263455231985"
+		};
+
+		initializeApp(firebaseConfig);
+	}
+}
