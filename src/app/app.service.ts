8ee4e5811aaee33ec6d5679b84584bc497eb48f1
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-30 01:16:53 +0800

    Added login function to service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-@Injectable({ providedIn: 'root' })
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+@Injectable({ providedIn: "root" })
 export class AppService {
+
+	constructor() { }
+
+	login(email: string, password: string) {
+		return Promise.resolve(firebase.auth().signInWithEmailAndPassword(email, password))
+			.catch((error) => Promise.reject(error));
+	}
 
 }
