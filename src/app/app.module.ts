9d9bc0235951005c720d8f4c0dca288bff5d21fd
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-30 01:17:13 +0800

    Removed previous firebase setup in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,11 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule, FirestoreSettingsToken } from "@angular/fire/firestore";
-
 import { AppComponent } from "./app.component";
 import { NavModule } from "./nav/nav.module";
 import { LoginModule } from "./login/login.module";
 
 const routes: Routes = [];
-
-const config = {
-	apiKey: "AIzaSyAqBGJy6mRnHjhU_YmeebSODruhfYHClj4",
-	authDomain: "budget-tracker-50645.firebaseapp.com",
-	databaseURL: "https://budget-tracker-50645.firebaseio.com",
-	projectId: "budget-tracker-50645",
-	storageBucket: "budget-tracker-50645.appspot.com",
-	messagingSenderId: "263455231985"
-};
 
 @NgModule({
 	declarations: [
@@ -30,16 +18,10 @@ const config = {
 		RouterModule.forRoot(routes),
 		BrowserAnimationsModule,
 
-		AngularFireModule.initializeApp(config),
-		AngularFirestoreModule,
-
 		NavModule,
 		LoginModule,
 	],
-	providers: [{
-		provide: FirestoreSettingsToken,
-		useValue: {},
-	}],
+	providers: [],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
