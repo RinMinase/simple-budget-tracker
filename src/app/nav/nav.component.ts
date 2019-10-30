2a6120f0479ecca50b54342966182c918f25cb3f
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-31 01:08:37 +0800

    Functional logout button

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
+import { AppService } from "@app/app.service";
+
 @Component({
 	selector: "app-nav",
 	styles: [
@@ -11,25 +13,39 @@ import { Router } from "@angular/router";
 		<mat-toolbar color="primary">
 			<span (click)="navigateHome()">Simple Budget Tracker</span>
 			<span class="spacer"></span>
-			<button mat-button (click)="navigateLogin()">
-				<span>Login</span>
+			<button mat-button (click)="navigateUser()">
+				<span *ngIf="!isLoggedIn">Login</span>
+				<span *ngIf="isLoggedIn">Logout</span>
 				<i class="material-icons ml">account_circle</i>
 			</button>
 		</mat-toolbar>`
 })
 export class NavComponent implements OnInit {
 
-	constructor(private router: Router) { }
+	isLoggedIn: boolean = false;
+
+	constructor(
+		private router: Router,
+		private service: AppService,
+	) { }
 
 	ngOnInit() {
+		this.service.currIsLoggedIn.subscribe((state) => this.isLoggedIn = state);
 	}
 
 	navigateHome() {
 		this.router.navigateByUrl("/")
 	}
 
-	navigateLogin() {
-		this.router.navigateByUrl("/login")
+	navigateUser() {
+		if (this.isLoggedIn) {
+			this.service.logout().then(() => {
+				this.service.setLoggedIn(false);
+				this.router.navigateByUrl("/login")
+			});
+		} else {
+			this.router.navigateByUrl("/login")
+		}
 	}
 
 }
