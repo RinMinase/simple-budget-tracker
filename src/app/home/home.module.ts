f6f04d31780b45e1c74c966fb142c966cc0f0eab
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-31 23:45:50 +0800

    Rearranged imports for readability

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from "./home.component";
-import { MatListModule, MatSidenavModule, MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatListModule, MatSidenavModule } from '@angular/material';
 
 @NgModule({
 	declarations: [HomeComponent],
