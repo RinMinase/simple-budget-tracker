cffe57958f93a83376eb7a79c0ae0ca04ea8ea54
Rin Minase <rin.black.raison.detre@gmail.com>
2019-11-01 00:35:17 +0800

    Functional currency in credit settings

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
 
 import { AppService } from '@app/app.service';
 
@@ -10,16 +12,34 @@ import { AppService } from '@app/app.service';
 })
 export class HomeComponent implements OnInit {
 
+	settingsCreditForm: FormGroup;
 	isSidenavOpen: boolean = true;
 	currActiveList: string = "Credit";
-	isSettingsOpen: boolean = false;
+	isSettingsOpen: boolean = true;
+
+	settings = {
+		currency: "PHP ",
+		credit: {
+			due: -1,
+			statement: -1
+		}
+	}
+
 
 	constructor(
+		private formBuilder: FormBuilder,
 		private router: Router,
+		private snackbar: MatSnackBar,
 		private service: AppService,
 	) { }
 
 	ngOnInit() {
+		this.settingsCreditForm = this.formBuilder.group({
+			currency: ["PHP", [Validators.maxLength(5)]],
+			due: [1, [Validators.required, Validators.min(1), Validators.max(30)]],
+			statement: [1, [Validators.required, Validators.min(1), Validators.max(30)]]
+		});
+
 		this.service.currIsSidenavOpen.subscribe((state) => this.isSidenavOpen = state);
 
 		this.service.auth()
@@ -28,6 +48,17 @@ export class HomeComponent implements OnInit {
 					this.router.navigateByUrl("/login")
 				}
 			});
+	}
+
+	saveCreditSettings() {
+		if (this.settingsCreditForm.valid) {
+			const { currency } = this.settingsCreditForm.value;
+
+			this.settings.currency = (currency) ? `${currency} ` : "";
+			this.isSettingsOpen = false;
+
+			this.snackbar.open("Settings saved", "Close", { duration: 2000 });
+		}
 	}
 
 	setActiveList(list: string) {
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,29 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from "./home.component";
-import { MatButtonModule, MatListModule, MatSidenavModule } from '@angular/material';
+import {
+	MatButtonModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatListModule,
+	MatSidenavModule,
+	MatSnackBarModule,
+} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [HomeComponent],
 	imports: [
 		CommonModule,
 		RouterModule.forChild([{ path: "", component: HomeComponent }]),
+		ReactiveFormsModule,
 
 		MatButtonModule,
+		MatFormFieldModule,
+		MatInputModule,
 		MatListModule,
 		MatSidenavModule,
+		MatSnackBarModule,
 	]
 })
 export class HomeModule { }
