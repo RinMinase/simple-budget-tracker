e65c8e31d3a47fdc42d3360b1e83af4875ee938c
Rin Minase <rin.black.raison.detre@gmail.com>
2019-11-01 02:13:05 +0800

    Removed unused config keys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,7 @@ export class AppComponent {
 	constructor(private service: AppService) {
 		const firebaseConfig = {
 			apiKey: "AIzaSyAqBGJy6mRnHjhU_YmeebSODruhfYHClj4",
-			authDomain: "budget-tracker-50645.firebaseapp.com",
-			databaseURL: "https://budget-tracker-50645.firebaseio.com",
 			projectId: "budget-tracker-50645",
-			storageBucket: "budget-tracker-50645.appspot.com",
-			messagingSenderId: "263455231985"
 		};
 
 		initializeApp(firebaseConfig);
