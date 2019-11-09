c55e40f976a3b01eacc5844b99fbf9673ec0f4db
Rin Minase <rin.black.raison.detre@gmail.com>
2019-11-10 03:38:49 +0800

    Merged main component to main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
+import { NgModule, Component } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { initializeApp } from "firebase/app";
 
-import { AppComponent } from "./app.component";
 import { NavModule } from "./nav/nav.module";
 import { LoginModule } from "./login/login.module";
 import { HomeModule } from "./home/home.module";
+import { AppService } from "@core/app.service";
+
+@Component({
+	selector: "app-root",
+	template: `<app-nav></app-nav><router-outlet></router-outlet>`,
+})
+class AppComponent {
+	constructor(private service: AppService) {
+		const firebaseConfig = {
+			apiKey: "AIzaSyAqBGJy6mRnHjhU_YmeebSODruhfYHClj4",
+			projectId: "budget-tracker-50645",
+		};
+
+		initializeApp(firebaseConfig);
+
+		this.service.auth()
+			.then((isAuth: boolean) => (isAuth) ? this.service.setLoggedIn(true) : null);
+	}
+}
 
 const routes: Routes = [];
 
